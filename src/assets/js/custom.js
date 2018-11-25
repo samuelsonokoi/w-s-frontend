34d814f290d266a3d17f7c95ea66877753f50ee7
Samuelson Okoi <samuelsonokoi@gmail.com>
2018-11-25 19:11:54 +0100

    added animation to task2

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -7,4 +7,6 @@ $(document).ready(function(){
   ScrollReveal().reveal('.show-off', { delay: 100, distance: '1em'});
   ScrollReveal().reveal('.f', { delay: 400, distance: '2em', opacity: 0.1});
   ScrollReveal().reveal('.s', { delay: 300, distance: '3em', opacity: 0.1});
+
+  ScrollReveal().reveal('.payment-form', { delay: 400, distance: '3em' });
 });
